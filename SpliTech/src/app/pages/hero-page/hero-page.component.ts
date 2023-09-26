09f23ad48f07e572745ac9ec06e6950de41e62ba
alilovicana <97240452+alilovicana@users.noreply.github.com>
2023-09-26 10:52:11 +0200

    main menu page responsivity done

--- SpliTech/src/app/pages/hero-page/hero-page.component.ts
+++ SpliTech/src/app/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { MainServiceService } from 'src/app/main-service.service';
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styleUrls: ['./hero-page.component.css']
 })
 export class HeroPageComponent {
-
+  constructor(public $service:MainServiceService){}
+  spliTech2023=this.$service.spliTech2023;
+  spliTech2024=this.$service.spliTech2024;
 }
